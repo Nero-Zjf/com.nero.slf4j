11331a27291ab272cd2b6433ae7c02dd8a12228f
ZengJunFeng <zengjunfeng9981@163.com>
2020-03-23 18:52:51 +0800

    初始提交

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# com.nero.slf4j
+# Slf4j日志框架 demo
+演示基本使用
+文档：Slf4j-介绍和基本使用
---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    /**
+     * 获取logger
+     */
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("slf4j demo");
+    }
+}
